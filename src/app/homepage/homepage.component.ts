99fa86e4505bd698814851f03b0180271eca6b24
Olha Shyshova <olha_shyshova@epam.com>
2021-12-12 01:24:17 +0200

    fix

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import { Board } from '../models/board.model';
 import { Column } from '../models/column.model';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+import { HomepageService } from './homepage.service';
 
 @Component({
   selector: 'app-homepage',
@@ -10,7 +13,12 @@ import { Column } from '../models/column.model';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    public auth: AuthService,
+    public homepageService: HomepageService,
+
+  ) { }
 
   board: Board = new Board('Test', [
 
@@ -27,6 +35,18 @@ export class HomepageComponent implements OnInit {
   ]);
 
   ngOnInit(): void { }  
+
+  addColumn(event: string) {
+    if (event) {
+      // this.homepageService.addColumn(event)
+    }
+  }
+
+  logout(event: Event) {
+    event.preventDefault();
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomepageComponent } from './homepage.component';
+
 import { DialogModule } from '../components/dialog/dialog.module';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { AppRoutingModule } from '../app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -11,8 +15,12 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
   ],
   imports: [
     CommonModule,    
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
     DialogModule,
-    DragDropModule
+    DragDropModule,
+    AppRoutingModule
   ],
   exports: [
     HomepageComponent
